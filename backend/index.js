18e4463221c2f223d68e8beb22f6dfb5c721da02
aakanshaa0 <aakanshapande23@gmail.com>
2025-05-08 02:27:13 +0530

    changes

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 const { userRouter } = require("./routes/user");
 const { courseRouter } = require("./routes/course");
@@ -9,8 +10,9 @@ const { adminRouter } = require("./routes/admin");
 
 const app = express();
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(express.json());
 
 console.log(process.env.MONGO_URL);
@@ -24,7 +26,7 @@ async function main() {
         await mongoose.connect(process.env.MONGO_URL);
 
     app.listen(PORT, ()=>{
-        console.log("Server is running on port 3000")
+        console.log(`Server is running on port ${PORT}`);
     });
     }
     catch(e){
